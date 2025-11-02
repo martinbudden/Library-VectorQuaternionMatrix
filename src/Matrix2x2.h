9bfa4b79b3d9438dfc012f6e3c26776f7745fce1
Martin Budden <mjbudden@gmail.com>
2025-11-02 09:47:55 +0000

    Added 2x2 Matrices.

---
+++ src/Matrix2x2.h
@@ -0,0 +1,101 @@
+#pragma once
+
+#include "xy_type.h"
+#include <array>
+#include <limits>
+
+class Matrix2x2 {
+public:
+    inline Matrix2x2() {}
+    inline explicit Matrix2x2(float diagonal) { _a.fill(0.0F); _a[0] = diagonal; _a[3] = diagonal; }
+    inline Matrix2x2(float d0, float d1) { _a.fill(0.0F); _a[0] = d0; _a[3] = d1; } //<! Set the matrix diagonal
+    inline explicit Matrix2x2(const std::array<float, 4>& a) : _a(a) {}
+    inline explicit Matrix2x2(const float a[4]) : _a({{ a[0], a[1], a[2], a[3] }}) {}
+    inline Matrix2x2(float a0, float a1, float a2, float a3) : _a({{ a0, a1, a2, a3 }}) {}
+public:
+    inline float operator[](size_t pos) const { return _a[pos]; } //<! Index operator
+    inline float& operator[](size_t pos) { return _a[pos]; } //<! Index operator
+
+    // Equality operators
+    inline bool operator!=(const Matrix2x2& m) const { for (size_t ii = 0; ii < _a.size(); ++ii) { if (_a[ii] != m[ii]) {return true;} } return false; } //<! Inequality operator
+    inline bool operator==(const Matrix2x2& m) const { return !operator!=(m); } //<! Equality operator
+
+    // Unary operations
+    inline Matrix2x2 operator+() const { return *this; } //<! Unary plus
+    inline Matrix2x2 operator-() const { Matrix2x2 m; for (size_t ii = 0; ii < _a.size(); ++ii) {m[ii] = -_a[ii];} return m; } //<! Unary negation
+
+    // cppcheck-suppress useStlAlgorithm
+    inline Matrix2x2 operator*=(float k) { for (float& a : _a) { a*=k; } return *this; } //<! Multiplication by a scalar
+    inline Matrix2x2 operator/=(float k) { const float r = 1.0F/k; return operator*=(r); } //<! Division by a scalar
+
+    inline Matrix2x2 operator+=(const Matrix2x2& m) { for (size_t ii = 0; ii < _a.size(); ++ii) {_a[ii] += m[ii];} return *this; } //<! Unary addition
+    inline Matrix2x2 operator-=(const Matrix2x2& m) { for (size_t ii = 0; ii < _a.size(); ++ii) {_a[ii] -= m[ii];} return *this; } //<! Unary subtraction
+    //! Unary multiplication
+    inline Matrix2x2 operator*=(const Matrix2x2& m) {
+        std::array<float, 4> a {{
+            _a[0]*m[0] + _a[1]*m[2],
+            _a[0]*m[1] + _a[1]*m[3],
+            _a[2]*m[0] + _a[3]*m[2],
+            _a[2]*m[1] + _a[3]*m[3],
+        }};
+        _a = a;
+        return *this;
+    }
+
+    // Binary operations
+    inline Matrix2x2 operator*(float k) const { Matrix2x2 m; for (size_t ii = 0; ii < _a.size(); ++ii) { m[ii] = _a[ii]*k; } return m; } //<! Multiplication by a scalar
+    inline friend Matrix2x2 operator*(float k, const Matrix2x2& m) { return m*k; } //<! Pre-multiplication by a scalar
+    inline Matrix2x2 operator/(float k) const { const float r = 1.0F/k; return *this*r; } //<! Division by a scalar
+
+    inline xy_t operator*(const xy_t& v) const { return xy_t { _a[0]*v.x + _a[1]*v.y, _a[2]*v.x + _a[3]*v.y }; } //<! Multiplication of a vector
+
+    inline Matrix2x2 operator+(const Matrix2x2& m) const { Matrix2x2 ret; for (size_t ii = 0; ii < _a.size(); ++ii) { ret[ii] = _a[ii] + m[ii]; } return ret; } //<! Addition
+    inline Matrix2x2 operator-(const Matrix2x2& m) const { Matrix2x2 ret; for (size_t ii = 0; ii < _a.size(); ++ii) { ret[ii] = _a[ii] - m[ii]; } return ret; } //<! Subtraction
+    //! Multiplication
+    inline Matrix2x2 operator*(const Matrix2x2& m) const {
+        return Matrix2x2(
+            _a[0]*m[0] + _a[1]*m[2],
+            _a[0]*m[1] + _a[1]*m[3],
+            _a[2]*m[0] + _a[3]*m[2],
+            _a[2]*m[1] + _a[3]*m[3]
+        );
+    }
+
+    inline void addToDiagonalInPlace(const xy_t& v) { _a[0]+=v.x; _a[3]+=v.y; } //<! Add vector to diagonal of matrix, in-place
+    inline void subtractFromDiagonalInPlace(const xy_t& v) { _a[0]-=v.x, _a[3]-=v.y; } //<! Subtract vector from diagonal of matrix, in-place
+    //! Multiply by matrix, in-place, assuming both matrices are diagonal
+     inline void multiplyAssumingDiagonalInPlace(const Matrix2x2& m) { _a[0]*=m[0]; _a[3]*=m[3]; }
+
+    inline Matrix2x2 addToDiagonal(const xy_t& v) const { return Matrix2x2 (_a[0]+v.x, _a[1], _a[2], _a[3]+v.y); } //<! Add vector to diagonal of matrix
+    inline Matrix2x2 subtractFromDiagonal(const xy_t& v) const { return Matrix2x2 (_a[0]-v.x, _a[1], _a[2], _a[3]-v.y); } //<! Subtract vector from diagonal of matrix
+    //! Multiply by matrix, assuming both matrices are diagonal
+    inline Matrix2x2 multiplyAssumingDiagonal(const Matrix2x2& m) const { return Matrix2x2 (_a[0]*m[0], 0.0F, 0.0F, _a[3]*m[3]); }
+
+    inline void setToIdentity() { _a.fill(0.0F); _a[0] = 1.0F; _a[3] = 1.0F; } //<! Sets matrix to identity matrix
+    inline void setToScaledIdentity(float d) { _a.fill(0.0F); _a[0] = d; _a[3] = d; } //<! Sets diagonal of matrix to d
+
+    inline void transposeInPlace() { float t = _a[1]; _a[1]= _a[2]; _a[2] = t; } //<! Transposes matrix, in=place
+    inline Matrix2x2 transpose() const { return Matrix2x2(_a[0], _a[2], _a[1], _a[3]); } //<! Returns transpose of matrix
+
+    //! Invert matrix, in-place
+    inline bool invertInPlace() {
+        const float det = _a[0]*_a[3] -_a[1]*_a[2];
+        if ((fabsf(det) <= std::numeric_limits<float>::epsilon())) {
+            return false;
+        }
+        const float t = _a[0];
+        _a[0] = _a[3]/det;
+        _a[1] = -_a[1]/det;
+        _a[2] = -_a[2]/det;
+        _a[3] = t/det;
+        return true;
+    }
+    inline Matrix2x2 inverse() const { Matrix2x2 ret = *this; (void)ret.invertInPlace(); return ret; } //<! Returns inverse of matrix
+
+    inline void invertInPlaceAssumingDiagonal() { _a[0] = 1.0F / _a[0]; _a[3] = 1.0F / _a[3]; } //<! Invert matrix in-place, assuming it is a diagonal matrix
+    inline Matrix2x2 inverseAssumingDiagonal() const { Matrix2x2 ret = *this; ret.invertInPlaceAssumingDiagonal(); return ret; } //<! Returns inverse of matrix, assuming it is diagonal
+
+    inline float determinant() const { return _a[0]*_a[3] -_a[1]*_a[2]; } //<! Matrix determinant
+protected:
+    std::array<float, 4> _a;
+};
---
+++ test/test_native/test_matrix2x2/test_matrix2x2.cpp
@@ -0,0 +1,257 @@
+#include "Matrix2x2.h"
+#include <unity.h>
+
+void setUp() {
+}
+
+void tearDown() {
+}
+
+// NOLINTBEGIN(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-init-variables,readability-magic-numbers)
+void test_readme()
+{
+    // test to check code example in readme.md compiles
+
+    const xy_t a{1.0F, 2.0F};
+    const xy_t b{5.0F, 7.0F};
+
+    // vector arithmetic
+    const xy_t c = a + b;
+    xy_t d = (a - b)*2.0F;
+    d += a;
+    d = c - d;
+
+    // vector dot and cross product
+    const float dotProduct = a.dot(b);
+
+    const Matrix2x2 M( 2,  3,
+                       7, 11);
+    const Matrix2x2 N(29, 31,
+                      41, 43);
+
+    // Matrix arithmetic
+    Matrix2x2 P = M*N;
+    P += M;
+    P *= 2;
+    P = P + N*M;
+
+    // Multiplication of a vector by a matrix
+    const xy_t v = P*a;
+
+    // dummy tests to avoid -Werror=unused-but-set-variable
+    const xy_t z = {0.0F, 0.0F};
+    const Matrix2x2 Z(0.0F);
+    TEST_ASSERT_FALSE(d == z);
+    TEST_ASSERT_FALSE(dotProduct == 0);
+    TEST_ASSERT_FALSE(P == Z);
+    TEST_ASSERT_FALSE(v == z);
+}
+
+void test_Matrix2x2_unary()
+{
+    // 0 1
+    // 2 3
+
+    std::array<float, 4> z;
+    z.fill(0.0F);
+    const Matrix2x2 Z(z);
+    const float f[4] = {  2,  3,  5,  7 }; // NOLINT(cppcoreguidelines-avoid-c-arrays,hicpp-avoid-c-arrays,modernize-avoid-c-arrays)
+    const Matrix2x2 F(f); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay,hicpp-no-array-decay)
+    TEST_ASSERT_EQUAL_FLOAT(2.0F, F[0]);
+    TEST_ASSERT_EQUAL_FLOAT(3.0F, F[1]);
+    TEST_ASSERT_EQUAL_FLOAT(7.0F, F[3]);
+
+
+    const Matrix2x2 I(1.0F);
+    TEST_ASSERT_TRUE(Z == Z * I);
+
+    TEST_ASSERT_TRUE(I == I); // NOLINT(misc-redundant-expression)
+    TEST_ASSERT_FALSE(I != I); // NOLINT(misc-redundant-expression)
+    TEST_ASSERT_TRUE(I == +I);
+    TEST_ASSERT_FALSE(I == -I);
+
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, I[0]);
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, I[3]);
+
+    TEST_ASSERT_EQUAL_FLOAT(0.0F, I[1]);
+    TEST_ASSERT_EQUAL_FLOAT(0.0F, I[2]);
+
+    Matrix2x2 IZ = Z;
+    IZ.setToIdentity();
+    TEST_ASSERT_TRUE(I == IZ);
+
+    const Matrix2x2 D2(2.0F);
+    const Matrix2x2 I2 = I * 2.0F;
+    TEST_ASSERT_TRUE(D2 == I2);
+
+    const Matrix2x2 I_NEG(-1.0F);
+    TEST_ASSERT_TRUE(I_NEG == -I);
+
+    //  2  3
+    //  5  7
+    const Matrix2x2 A  ( 2, 3, 5, 7);
+    TEST_ASSERT_EQUAL_FLOAT(-1.0F, A.determinant());
+    const Matrix2x2 ATE( 2, 5, 3, 7);
+
+    Matrix2x2 A_plus_A = A;
+    A_plus_A += A;
+    TEST_ASSERT_TRUE(A_plus_A == A*2.0F);
+    TEST_ASSERT_TRUE(A_plus_A == A + A);
+
+    TEST_ASSERT_TRUE(A == A * I);
+    TEST_ASSERT_TRUE(A == I * A);
+
+    const Matrix2x2 AT = A.transpose();
+    TEST_ASSERT_TRUE(ATE == AT);
+    const Matrix2x2 ATT = AT.transpose();
+    TEST_ASSERT_TRUE(ATT == A);
+    Matrix2x2 A2 = A;
+    A2.transposeInPlace();
+    TEST_ASSERT_TRUE(ATE == A2);
+
+    const Matrix2x2 B(29, 31, 37, 41);
+    Matrix2x2 A_plus_B = A;
+    A_plus_B += B;
+    Matrix2x2 B_plus_A = B;
+    B_plus_A += A;
+    TEST_ASSERT_TRUE(A_plus_B == B_plus_A);
+    TEST_ASSERT_TRUE(A_plus_B == A + B);
+    TEST_ASSERT_TRUE(A_plus_B == B + A);
+
+    const Matrix2x2 C(29, -131, 537, -41);
+    TEST_ASSERT_EQUAL(69158.0F, C.determinant());
+    Matrix2x2 C_inv = C;
+
+    C_inv.invertInPlace();
+    TEST_ASSERT_EQUAL_FLOAT(-41.0F/69158.0F, C_inv[0]);
+    TEST_ASSERT_EQUAL_FLOAT(131.0F/69158.0F, C_inv[1]);
+    TEST_ASSERT_EQUAL_FLOAT(-537.0F/69158.0F, C_inv[2]);
+    TEST_ASSERT_EQUAL_FLOAT(29.0F/69158.0F, C_inv[3]);
+    const Matrix2x2 C_times_C_inv = C * C_inv;
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, C_times_C_inv[0]);
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, C_times_C_inv[3]);
+
+    TEST_ASSERT_FLOAT_WITHIN(1e-07, 0.0F, C_times_C_inv[1]);
+    TEST_ASSERT_FLOAT_WITHIN(1e-07, 0.0F, C_times_C_inv[2]);
+
+    const Matrix2x2 C_inv2 = C.inverse();
+    TEST_ASSERT_TRUE(C_inv == C_inv2);
+
+    TEST_ASSERT_TRUE(A * B != B * A);
+
+    Matrix2x2 A_times_B = A;
+    A_times_B *= B;
+    Matrix2x2 B_times_A = B;
+    B_times_A *= A;
+    TEST_ASSERT_TRUE(A_times_B == A * B);
+    TEST_ASSERT_TRUE(B_times_A == B * A);
+
+    const float d = A.determinant();
+    const float dE =  A[0]*A[3] - A[1]*A[2];
+    TEST_ASSERT_EQUAL_FLOAT(dE, d);
+
+    const Matrix2x2 D(29, 0, 0, 31);
+    Matrix2x2 D_inv = D;
+    D_inv.invertInPlaceAssumingDiagonal();
+    TEST_ASSERT_EQUAL_FLOAT(1.0F / 29.0F, D_inv[0]);
+    TEST_ASSERT_EQUAL_FLOAT(1.0F / 31.0F, D_inv[3]);
+
+    const Matrix2x2 D_times_D_inv = D * D_inv;
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, D_times_D_inv[0]);
+    TEST_ASSERT_EQUAL_FLOAT(1.0F, D_times_D_inv[3]);
+
+    TEST_ASSERT_EQUAL_FLOAT(0.0F, D_times_D_inv[1]);
+    TEST_ASSERT_EQUAL_FLOAT(0.0F, D_times_D_inv[2]);
+
+    const Matrix2x2 D_inv2 = D.inverseAssumingDiagonal();
+    TEST_ASSERT_EQUAL_FLOAT(1.0F / 29.0F, D_inv2[0]);
+    TEST_ASSERT_EQUAL_FLOAT(1.0F / 31.0F, D_inv2[3]);
+    TEST_ASSERT_TRUE(D_inv == D_inv2);
+
+    const xy_t v{2, 3};
+    const Matrix2x2 DplusV = D.addToDiagonal(v);
+    TEST_ASSERT_EQUAL_FLOAT(31, DplusV[0]);
+    TEST_ASSERT_EQUAL_FLOAT(34, DplusV[3]);
+    Matrix2x2 DplusVinPlace = D; // NOLINT(misc-const-correctness) false positive
+    DplusVinPlace.addToDiagonalInPlace(v);
+    TEST_ASSERT_TRUE(DplusV == DplusVinPlace);
+
+    const Matrix2x2 DminusV = D.subtractFromDiagonal(v);
+    TEST_ASSERT_EQUAL_FLOAT(27, DminusV[0]);
+    TEST_ASSERT_EQUAL_FLOAT(28, DminusV[3]);
+    Matrix2x2 DminusVinPlace = D; // NOLINT(misc-const-correctness) false positive
+    DminusVinPlace.subtractFromDiagonalInPlace(v);
+    TEST_ASSERT_TRUE(DminusV == DminusVinPlace);
+
+    const Matrix2x2 DminusVplusV = DminusV.addToDiagonal(v);
+    TEST_ASSERT_TRUE(D == DminusVplusV);
+
+    const Matrix2x2 E(2, 0, 0, 3);
+    const Matrix2x2 DtimesE(29*2, 0, 0, 31*3);
+    TEST_ASSERT_TRUE(DtimesE == D.multiplyAssumingDiagonal(E));
+
+    Matrix2x2 DtimesEinPlace = D;
+    DtimesEinPlace.multiplyAssumingDiagonalInPlace(E);
+    TEST_ASSERT_TRUE(DtimesE == DtimesEinPlace);
+}
+
+void test_Matrix2x2_binary()
+{
+    const Matrix2x2 A(  2,  3,
+                        5,  7);
+    const Matrix2x2 B(29, 31,
+                      37, 41);
+    const Matrix2x2 A_times_B(
+        2*29 +  3*37,   2*31 +  3*41,
+        5*29 +  7*37, + 5*31 +  7*41);
+    TEST_ASSERT_TRUE(A_times_B == A * B);
+
+    const Matrix2x2 A_plus_B (2+29, 3+31, 5+37, 7+41);
+    const Matrix2x2 A_minus_B(2-29, 3-31, 5-37, 7-41);
+
+    TEST_ASSERT_TRUE(A_plus_B == A + B);
+    TEST_ASSERT_TRUE(A_plus_B == B + A);
+    TEST_ASSERT_TRUE(A_minus_B == A - B);
+    TEST_ASSERT_TRUE(-A_minus_B == B - A);
+
+    const float k = 4.0F;
+    const Matrix2x2 Ak(2*k, 3*k, 5*k, 7*k);
+    TEST_ASSERT_TRUE(Ak == A*k);
+    TEST_ASSERT_TRUE(Ak == k*A);
+    TEST_ASSERT_TRUE(A == Ak/k);
+    Matrix2x2 Ake = A;
+    Ake *= k;
+    TEST_ASSERT_TRUE(Ak == Ake);
+    Ake /= k;
+    TEST_ASSERT_TRUE(A == Ake);
+
+    const xy_t v  = {29, 31};
+    const xy_t Av = {2*29 + 3*31, 5*29 + 7*31};
+    TEST_ASSERT_TRUE(Av == A*v);
+
+    //const Matrix2x2 A(  2,  3,
+    //                    5,  7);
+    const Matrix2x2 D(67,  0, 
+                       0, 71);
+    const Matrix2x2 DA = D * A;
+    TEST_ASSERT_EQUAL_FLOAT(2 * 67, DA[0]);
+    TEST_ASSERT_EQUAL_FLOAT(3 * 67, DA[1]);
+    TEST_ASSERT_EQUAL_FLOAT(5 * 71, DA[2]);
+    TEST_ASSERT_EQUAL_FLOAT(7 * 71, DA[3]);
+}
+
+// NOLINTEND(cppcoreguidelines-avoid-magic-numbers,cppcoreguidelines-init-variables,readability-magic-numbers)
+
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+
+    UNITY_BEGIN();
+
+    RUN_TEST(test_readme);
+    RUN_TEST(test_Matrix2x2_unary);
+    RUN_TEST(test_Matrix2x2_binary);
+
+    UNITY_END();
+}
